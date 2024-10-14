3ff030199189438e9b3c0b8541f680eca1c2c69f
lemon-cake <kaique-lemos@hotmail.com>
2024-10-14 16:31:21 +0200

    exo 2

--- Exercice-assurance/script.js
+++ Exercice-assurance/script.js
@@ -105,5 +105,74 @@ if (isOldMember === true){
 console.log(TarifClient);
 
 
+//-------------------------------------------------------------//
+
+// exo Photocopie -----
+
+/*problem to solve :
+
+-photocopie coute 10 cents, 9 cents les vingts et 20> = 8 cents
+
+*Nombre de photocopies
+
+*/
+
+// init la variable 
+
+let Nphotocopies = 31
+
+// test 1:
+
+// function PrixPhotocopie(Nphotocopies){
+//     let prix = 0;
+
+//     if (Nphotocopies > 10 ){
+//         prix = Nphotocopies*0.1;
+//     } else if (Nphotocopies > 30) {
+//         prix = Nphotocopies*0.1;
+//         Nphotocopies-10;
+//         prix = Nphotocopies*.09 + 1;
+//     } else if (Nphotocopies > 20){
+
+//     }
+//     console.log(prix);
+//     return prix;
+// }
+
+// test 2:
+
+function PrixPhotocopie(Nphotocopies){
+    let prix = 0;
+    while (Nphotocopies>30){
+        prix += 0.08;
+        Nphotocopies--;
+        //console.log(Nphotocopies);
+    }
+    //console.log(prix); // works
+    
+    while (Nphotocopies>10){
+        Nphotocopies--;
+        //console.log(Nphotocopies);
+        prix += 0.09;
+    }
+
+    while (Nphotocopies>0){
+        Nphotocopies--;
+        //console.log(Nphotocopies);
+        prix += 0.1;
+    }
+
+    //.toFixed() 
+    return prix.toFixed(2);
+}
+
+
+console.log(PrixPhotocopie(Nphotocopies));
+
+//console.log(prix);
+
+
+
+
 
 
