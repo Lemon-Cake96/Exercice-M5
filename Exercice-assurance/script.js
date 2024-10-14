1266e033e42ccd5a51e704b129230ee8668c63a8
lemon-cake <kaique-lemos@hotmail.com>
2024-10-14 10:17:48 +0200

    probleme mis en page

--- Exercice-assurance/script.js
+++ Exercice-assurance/script.js
@@ -1,3 +1,31 @@
 /* Problem to solve :
 
--We have 4 different prices,
+-We have 4 different prices, each have their own conditions based on :
+*Age of conductor
+    *Below 25 years
+    *Above 25
+*Age of license
+    *Below 2 years
+    *Above 2 ""
+*Number of Accidents
+*Contract > 12m = cheaper tier
+
+-We have 5 tiers of prices (from cheapest to expensive):
+*A
+*B
+*C
+*D
+*Refusé
+
+*/
+
+//init neccesary variales
+
+let AgeBelow25 = false;
+let AgeLicenseAbove2 = false;
+let NumberOfAccidents = 0;
+let isOldMember = false;  //Has the client been assured for over a year
+
+
+
+
