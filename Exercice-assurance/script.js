135fb6eec6fc3511b28babdeb74761ddd097ade8
lemon-cake <kaique-lemos@hotmail.com>
2024-10-21 10:00:46 +0200

    Problem : elodie stays at the end

--- Exercice-assurance/script.js
+++ Exercice-assurance/script.js
@@ -346,14 +346,10 @@ let memoire;
 //creation de la boucle
 
 function Trieur(array){
-    //elementArriere = undefined;
-    //elementDevant = undefined;
     let memoire;
-    for (let i=0; i< array.length;i++ ){ // it might not fully loop thro the whole array after a swap ?
+    for (let i=0; i< array.length;i++ ){ // it might not fully loop thro the whole array after a swap ? fixed
         //console.log(i);
         //console.log(i+1);
-        // elementArriere = array[i];
-        // elementDevant = array[i+1]; //no need
         for (let k = 0; k < array.length; k++){ //new loop
             if (array[k] > array[k+1]){
                 memoire = array[k];
@@ -629,6 +625,110 @@ let etudiants = [
     { nom:"Amin", note:17},
     { nom:"Yohan", note:18},
 ];
+// New student list :
+
+const NewEtudiants = [
+    { nom: "Alice", note: 85 },
+    { nom: "benjamin", note: 92 },
+    { nom: "Claire", note: 78 },
+    { nom: "david", note: 88 },
+    { nom: "Emma", note: 95 },
+    { nom: "fabien", note: 67 },
+    { nom: "Gabriel", note: 90 },
+    { nom: "helene", note: 73 },
+    { nom: "Isabelle", note: 82 },
+    { nom: "jacques", note: 76 },
+    { nom: "Kevin", note: 89 },
+    { nom: "laura", note: 91 },
+    { nom: "Marc", note: 84 },
+    { nom: "nathalie", note: 79 },
+    { nom: "Olivier", note: 87 },
+    { nom: "pierre", note: 93 },
+    { nom: "Quentin", note: 80 },
+    { nom: "roxane", note: 77 },
+    { nom: "Sophie", note: 94 },
+    { nom: "thomas", note: 86 },
+    { nom: "Ulysse", note: 81 },
+    { nom: "valerie", note: 75 },
+    { nom: "William", note: 83 },
+    { nom: "xavier", note: 70 },
+    { nom: "Yann", note: 88 },
+    { nom: "zoe", note: 92 },
+    { nom: "Adrien", note: 85 },
+    { nom: "brigitte", note: 78 },
+    { nom: "Camille", note: 89 },
+    { nom: "damien", note: 91 },
+    { nom: "Elise", note: 87 },
+    { nom: "françois", note: 76 },
+    { nom: "Gaëlle", note: 84 },
+    { nom: "hugo", note: 93 },
+    { nom: "Ingrid", note: 79 },
+    { nom: "julien", note: 88 },
+    { nom: "Karine", note: 82 },
+    { nom: "lucas", note: 90 },
+    { nom: "Mathilde", note: 95 },
+    { nom: "nicolas", note: 77 },
+    { nom: "Océane", note: 86 },
+    { nom: "paul", note: 80 },
+    { nom: "Quentin", note: 83 },
+    { nom: "rachel", note: 75 },
+    { nom: "Samuel", note: 92 },
+    { nom: "thibault", note: 89 },
+    { nom: "Ursula", note: 78 },
+    { nom: "vincent", note: 91 },
+    { nom: "Wendy", note: 87 },
+    { nom: "xena", note: 76 },
+    { nom: "Yasmine", note: 84 },
+    { nom: "zacharie", note: 93 },
+    { nom: "Amélie", note: 79 },
+    { nom: "bruno", note: 88 },
+    { nom: "Chloé", note: 82 },
+    { nom: "dylan", note: 90 },
+    { nom: "Estelle", note: 95 },
+    { nom: "fabrice", note: 77 },
+    { nom: "Géraldine", note: 86 },
+    { nom: "henri", note: 80 },
+    { nom: "Inès", note: 83 },
+    { nom: "jean", note: 75 },
+    { nom: "Kévin", note: 92 },
+    { nom: "léa", note: 89 },
+    { nom: "Mélanie", note: 78 },
+    { nom: "nathan", note: 91 },
+    { nom: "Ophélie", note: 87 },
+    { nom: "patrick", note: 76 },
+    { nom: "Quentin", note: 84 },
+    { nom: "rémi", note: 93 },
+    { nom: "Sarah", note: 79 },
+    { nom: "théo", note: 88 },
+    { nom: "Ugo", note: 82 },
+    { nom: "valentin", note: 90 },
+    { nom: "Wendy", note: 95 },
+    { nom: "xavier", note: 77 },
+    { nom: "Yohan", note: 86 },
+    { nom: "zoé", note: 80 },
+    { nom: "Antoine", note: 83 },
+    { nom: "bernadette", note: 75 },
+    { nom: "Cédric", note: 92 },
+    { nom: "diane", note: 89 },
+    { nom: "Élodie", note: 78 },
+    { nom: "franck", note: 91 },
+    { nom: "Guillaume", note: 87 },
+    { nom: "hélène", note: 76 },
+    { nom: "Isabelle", note: 84 },
+    { nom: "jules", note: 93 },
+    { nom: "Kevin", note: 79 },
+    { nom: "laetitia", note: 88 },
+    { nom: "Maxime", note: 82 },
+    { nom: "nadine", note: 90 },
+    { nom: "Olivier", note: 95 },
+    { nom: "pascal", note: 77 },
+    { nom: "Quentin", note: 86 },
+    { nom: "romain", note: 80 },
+    { nom: "Stéphanie", note: 83 },
+    { nom: "thomas", note: 75 },
+    { nom: "Ulysse", note: 92 },
+    { nom: "valérie", note: 89 }
+];
 
 //Keep ones with note > 15 
 
@@ -647,8 +747,8 @@ console.log(GoodEtudiants);
 
 //step 1: convert everything to lower case, cant use methods.
 
-let Alpha = "abcdefghijklmnopqrstuvwxyz";
-let UAlpha ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let Alpha = "abcdeéèfghijklmnopqrstuvwxyz";
+let UAlpha ="ABCDEÉÈFGHIJKLMNOPQRSTUVWXYZ";
 
 function LetterConvMiniscule(letter){
     for (let i=0; i < UAlpha.length; i++){
@@ -685,33 +785,35 @@ function WordConvMinscule(word){
 
 //step 3 now that we can convert every name to lower case we can compare them
 
-function TrieurEtudiants(GoodEtudiants) {
+function TrieurEtudiants(array) {
     //let name1;
     //let name2;
     let memo;
-    for (let i = 0; i < GoodEtudiants.length; i++){
-        console.log(i);
+    for (let i = 0; i < array.length; i++){
+        //console.log(i);
         //console.log(WordConvMinscule(GoodEtudiants[i].nom)); // works
-        for (let j = 0; j < GoodEtudiants.length - 1; j++){
-            // name1 = WordConvMinscule(GoodEtudiants[j].nom);
-            // name2 = WordConvMinscule(GoodEtudiants[j + 1].nom);
+        for (let l = 0; l < array.length - 1; l++){
+            // name1 = WordConvMinscule(GoodEtudiants[l].nom);
+            // name2 = WordConvMinscule(GoodEtudiants[l + 1].nom);
             // console.log(name1);
             // console.log(name2);
-            if (name1 > name2){
-                memo = name1;
-                name1 = name2;
-                name2 = memo;
-                //
-                
-
+            if (WordConvMinscule(array[l].nom) > WordConvMinscule(array[l+1].nom)){
+                // console.log(l); //works
+                // console.log(WordConvMinscule(array[l].nom));
+                // console.log(WordConvMinscule(array[l+1].nom));
+                memo = array[l]; /*earlier version: memo = array[l].nom trying to re assign only
+                the names of each member and not the whole student (NAME and NOTES)*/
+                array[l] = array[l+1];
+                array[l+1] = memo;
             }
 
         }
     }
-    return GoodEtudiants;
+    return array;
 }
 
 console.log(TrieurEtudiants(GoodEtudiants));
+console.log(TrieurEtudiants(NewEtudiants));
 
 
 
